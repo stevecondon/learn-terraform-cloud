1b06d8cec5c5626f13d83540e7b4d9ec52a81663
stevecondon <109669733+stevecondon@users.noreply.github.com>
2022-07-20 15:13:26 +0100

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,39 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+  cloud {
+    organization = "stevecondon"
+    workspaces {
+      name = "learn-terraform-azure"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "myTFResourceGroup"
+  location = "westeurope"
+  tags = {
+    Environment = "Terraform Getting Started"
+    Team = "DevOps"
+  }
+}
+
+# Create a virtual network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "myTFVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = "westus2"
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
